519e56f004dbd204b1da10c17ffdec328df23754
Xander Stoffels <xander.s@outlook.com>
2019-09-21 21:32:10 +0200

    Added Swashbuckle

--- StatApi/Startup.cs
+++ StatApi/Startup.cs
@@ -11,12 +11,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using StatApi.Data;
 
 namespace StatApi
 {
     public class Startup
     {
+        private static readonly string VERSION = "v1.0";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -33,6 +36,20 @@ namespace StatApi
                 builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(VERSION, new OpenApiInfo
+                {
+                    Title = "Twitch Analysis Api",
+                    Version = VERSION,
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Xander Stoffels",
+                        Email = "xander.s@outlook.com",
+                        Url = new Uri("https://github.com/XanderStoffels/twitch-analyser")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,8 +64,11 @@ namespace StatApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", $"Twitch Analysis Api {VERSION}"); });
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
